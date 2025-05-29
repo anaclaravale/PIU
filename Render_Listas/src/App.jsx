c09d0811ca701ec8a1b0e10ce46bb431c6983cae
anaclaravale <anaclararn06@gmail.com>
2025-05-29 14:24:27 -0300

    Início react_listas

---
+++ Render_Listas/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import ListasMap from './components/ListasMap'
+import ListasFilter from './components/ListasFilter'
+import ListasObj from './components/ListasObj'
+
+function App() {
+
+  return (
+    <>
+     <ListasMap />
+     <br />
+     <ListasFilter/>
+     <br />
+     <ListasObj/>
+    </>
+  )
+}
+
+export default App
